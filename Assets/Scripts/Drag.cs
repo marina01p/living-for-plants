7b02fdd698e17f956e64755ea3ab67e00d2eb308
marina <marina.petici1@gmail.com>
2024-06-20 09:16:40 +0300

    btn functionality

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -7,31 +7,32 @@ public class Drag : MonoBehaviour
     private float distance;
     private Vector3 initialPosition;
 
-    public GameObject dropZoneOne;
-    public GameObject dropZoneTwo;
+    private GameObject dropZoneOne;
+    private GameObject dropZoneTwo;
+
+    void Start()
+    {
+        dropZoneOne = GameObject.Find("shelf_one");
+        dropZoneTwo = GameObject.Find("shelf_two");
+    }
 
     void OnMouseDown()
     {
-        Debug.Log("OnMouseDown called");
         distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         dragging = true;
-        initialPosition = transform.position; // Store the initial position
+        initialPosition = transform.position;
     }
 
     void OnMouseUp()
     {
-        Debug.Log("OnMouseUp called");
         dragging = false;
-        // Check if the object is within any of the drop zones
         if (!IsWithinDropZone(transform.position, dropZoneOne) && !IsWithinDropZone(transform.position, dropZoneTwo))
         {
-            Debug.Log("Dropped outside valid zones, returning to initial position");
-            // If not, reset to initial position
             transform.position = initialPosition;
         }
         else
         {
-            Debug.Log("Dropped inside a valid zone");
+            Debug.Log("not a valid zone");
         }
     }
 
@@ -47,20 +48,8 @@ public class Drag : MonoBehaviour
 
     private bool IsWithinDropZone(Vector3 position, GameObject dropZone)
     {
-        if (dropZone == null)
-        {
-            Debug.LogError("Drop zone not assigned.");
-            return false;
-        }
-
         Collider2D collider = dropZone.GetComponent<Collider2D>();
-        if (collider == null)
-        {
-            Debug.LogError("Drop zone does not have a Collider2D component.");
-            return false;
-        }
-        bool withinBounds = collider.bounds.Contains(position);
-        Debug.Log("Checking position: " + position + " within bounds of " + dropZone.name + ": " + withinBounds);
+        bool withinBounds = collider.OverlapPoint(position);
         return withinBounds;
     }
 }
